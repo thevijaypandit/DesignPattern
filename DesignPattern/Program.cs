5383ff6b2842a2e5d3d16606b1484e6d0cc8407c
thevijaypandit <thevijaypandit@gmail.com>
2020-02-14 12:47:23 +0545

    adding git ignore

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using DesignPattern.SingletonDesignPattern;
+using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region singleton
+            Singleton.GetInstance.Show();
+            Singleton.GetInstance.Show();
+            Singleton.GetInstance.Show();
+            #endregion
+            #region factory pattern
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPattern/SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.SingletonDesignPattern
+{
+    public class Singleton
+    {
+        // .NET guarantees thread safety for static initialization
+        private static Singleton singletonInstance = null;
+        // Lock synchronization object
+        private static object syncLock = new object();
+        public string Name { get; set; }
+        public string IP { get; set; }
+        private Singleton()
+        {
+            //To DO: Remove below line
+            Console.WriteLine("Singleton Intance");
+            this.Name = "Server1";
+            this.IP = "192.168.3.222";
+        }
+        public void Show()
+        {
+            Console.WriteLine("Server Information is : Name={0} & IP={1}", Name,IP);
+        }
+        public static Singleton GetInstance
+        {
+            //get
+            //{
+            //    // Support multithreaded applications through
+            //    // 'Double checked locking' pattern which (once
+            //    // the instance exists) avoids locking each
+            //    // time the method is invoked
+            //    lock (syncLock)
+            //    {
+            //        if (Singleton.instance == null)
+            //            Singleton.instance = new Singleton();
+
+            //        return Singleton.instance;
+            //    }
+            //}
+            get
+            {
+                lock (syncLock)
+                {
+                    if (singletonInstance == null)
+                    {
+                        singletonInstance = new Singleton();
+                    }
+                    return singletonInstance;
+                }
+            }
+        }
+    }
+}
