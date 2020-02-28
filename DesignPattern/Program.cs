bb7be3aeecbbeae75bf42e1c22a043cb8b255a62
thevijaypandit <thevijaypandit@gmail.com>
2020-02-28 12:32:35 +0545

    change in program

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -4,6 +4,8 @@ using DesignPattern.PrototypeDesignPattern;
 using DesignPattern.SingletonDesignPattern;
 using SolidPrinciple.DependencyInversionPrinciple_DIP_;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DesignPattern
 {
@@ -12,6 +14,9 @@ namespace DesignPattern
 
         static void Main(string[] args)
         {
+            var evenList = new List<int>();
+            var oddList = new List<int>();
+            var oddEvenList = new List<int>();
             #region singleton
             //Singleton.GetInstance.Show();
             //Singleton.GetInstance.Show();
@@ -62,6 +67,25 @@ namespace DesignPattern
 
             #region SOLID Design Principle
             #region DependencyInversionPrinciple
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    oddList.Add(10-i);
+                }
+
+                else if (i % 2 != 0)
+                {
+                    evenList.Add(i);
+                }
+                //oddEvenList = oddList.Concat(evenList).ToList();
+                oddEvenList.Add(i);
+
+            }
+            foreach (var n in oddEvenList)
+            {
+                Console.WriteLine(n);
+            }
             IAutomobile automobile = new Jeep();
             AutomobileController automobileController = new AutomobileController(automobile);
             automobile.Ignition();
